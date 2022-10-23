e4c981ce13532adf674d2c2f0b6858ab2b728d63
Schauweg <felix.hess@freenet.de>
2022-10-23 03:04:40 +0200

    Update to 1.19.2

--- src/main/java/schauweg/unsuspiciousstew/StewInfo.java
+++ src/main/java/schauweg/unsuspiciousstew/StewInfo.java
@@ -1,27 +1,24 @@
 package schauweg.unsuspiciousstew;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffectUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.ChatUtil;
 import net.minecraft.util.Formatting;
 
 import java.util.List;
 
 public class StewInfo {
 
-    static MinecraftClient minecraft = MinecraftClient.getInstance();
-
     public static void onInjectTooltip(Object stackIn, List<Text> list) {
         ItemStack stack = (ItemStack) stackIn;
         if (stack != null && (stack.getItem() == Items.SUSPICIOUS_STEW)){
-            NbtCompound tag = stack.getTag();
+            NbtCompound tag = stack.getNbt();
             if (tag != null) {
                 NbtList effects = tag.getList("Effects", 10);
                 int effectsCount = effects.size();
@@ -30,8 +27,11 @@ public class StewInfo {
                     tag = effects.getCompound(i);
                     int duration = tag.getInt("EffectDuration");
                     StatusEffect effect = StatusEffect.byRawId(tag.getByte("EffectId"));
-                    list.add(new TranslatableText(effect.getTranslationKey())
-                            .append(" "+ChatUtil.ticksToString(duration))
+
+                    StatusEffectInstance effectInstance = new StatusEffectInstance(effect, duration);
+
+                    list.add(Text.translatable(effect.getTranslationKey())
+                            .append(" "+ StatusEffectUtil.durationToString(effectInstance, 1))
                             .setStyle(Style.EMPTY.withColor(Formatting.GRAY)));
                 }
             }
--- src/main/java/schauweg/unsuspiciousstew/mixin/MixinInGameHud.java
+++ src/main/java/schauweg/unsuspiciousstew/mixin/MixinInGameHud.java
@@ -7,28 +7,22 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffectUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.text.*;
-import net.minecraft.util.ChatUtil;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import schauweg.unsuspiciousstew.StewInfo;
-
-import java.util.List;
 
 import static net.minecraft.client.gui.DrawableHelper.fill;
 
@@ -50,23 +44,23 @@ public abstract class MixinInGameHud {
     private int scaledWidth;
 
     @Shadow
-    public abstract TextRenderer getFontRenderer();
-
-    @Shadow
     private int scaledHeight;
+
+
+    @Shadow public abstract TextRenderer getTextRenderer();
 
     @Inject(at = @At("HEAD"), method = "renderHeldItemTooltip")
     public void onInjectTooltip(MatrixStack matrixStack, CallbackInfo info) {
 
         this.client.getProfiler().push("selectedItemName");
         if (this.heldItemTooltipFade > 0 && !this.currentStack.isEmpty()) {
-            MutableText mutableText = (new LiteralText("")).append(this.currentStack.getName()).formatted(this.currentStack.getRarity().formatting);
+            MutableText mutableText = Text.empty().append(this.currentStack.getName()).formatted(this.currentStack.getRarity().formatting);
             if (this.currentStack.hasCustomName()) {
                 mutableText.formatted(Formatting.ITALIC);
             }
 
-            int mainItemNameWidth = this.getFontRenderer().getWidth(mutableText);
-            int j = (this.scaledWidth - mainItemNameWidth) / 2;
+            int mainItemNameWidth = this.getTextRenderer().getWidth(mutableText);
+            int textWidth = (this.scaledWidth - mainItemNameWidth) / 2;
             int hotbarOffset = this.scaledHeight - 59;
             if (!this.client.interactionManager.hasStatusBars()) {
                 hotbarOffset += 14;
@@ -81,13 +75,13 @@ public abstract class MixinInGameHud {
                 matrixStack.push();
                 RenderSystem.enableBlend();
                 RenderSystem.defaultBlendFunc();
-                int var10001 = j - 2;
+                int var10001 = textWidth - 2;
                 int var10002 = hotbarOffset - 2;
-                int var10003 = j + mainItemNameWidth + 2;
-                this.getFontRenderer().getClass();
+                int var10003 = textWidth + mainItemNameWidth + 2;
+                this.getTextRenderer().getClass();
                 fill(matrixStack, var10001, var10002, var10003, hotbarOffset + 9 + 2, this.client.options.getTextBackgroundColor(0));
                 if (currentStack.getItem() == Items.SUSPICIOUS_STEW){
-                    NbtCompound tag = currentStack.getTag();
+                    NbtCompound tag = currentStack.getNbt();
                     if (tag != null) {
                         NbtList effects = tag.getList("Effects", 10);
                         int effectsCount = effects.size();
@@ -95,10 +89,14 @@ public abstract class MixinInGameHud {
                             tag = effects.getCompound(i);
                             int duration = tag.getInt("EffectDuration");
                             StatusEffect effect = StatusEffect.byRawId(tag.getByte("EffectId"));
-                            String time = ChatUtil.ticksToString(duration);
-                            Text completeText = new TranslatableText(effect.getTranslationKey()).append(" "+time);
-                            j = (this.scaledWidth - getFontRenderer().getWidth(completeText)) / 2;
-                            this.getFontRenderer().drawWithShadow(matrixStack, completeText, (float)j, (float)hotbarOffset-(i*14)-14, 13421772 + (opacity << 24));
+                            if (effect != null) {
+                                StatusEffectInstance effectInstance = new StatusEffectInstance(effect, duration);
+                                String time = StatusEffectUtil.durationToString(effectInstance, 1);
+                                Text completeText = Text.translatable(effect.getTranslationKey()).append(" "+time);
+                                textWidth = (this.scaledWidth - getTextRenderer().getWidth(completeText)) / 2;
+                                this.getTextRenderer().drawWithShadow(matrixStack, completeText, (float)textWidth, (float)hotbarOffset-(i*14)-14, 13421772 + (opacity << 24));
+                            }
+
                         }
                     }
                 }
